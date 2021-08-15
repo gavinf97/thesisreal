9ab08e1c9cb5a1751461c7e84b27fcf44fffed46
gavinf97 <72207684+gavinf97@users.noreply.github.com>
2021-08-15 03:13:39 +0100

    Delete Bash_scripts/bashscripts_file_wrangling directory

--- Bash_scripts/bashscripts_file_wrangling/gbk_counter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Gavin Farrell - 15/8/21
-
-#Use:
-#File management
-
-#Description:
-#Counts all files ending with '.gbk' (GenBank) file extension in current  directory
-ls *.gbk | wc -l
--- Bash_scripts/bashscripts_file_wrangling/remove_empty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Gavin Farrell – 15/8/21 
-
-#Use: 
-#File management 
-
-#Description: 
-#Removes all empty size 0 files (useful for incomplete antismash runs; gbk/fasta files/etc... 
-find ./ -size  0 -print -delete
